713192d3195796f800ba5b7c87fc27b8796580fb
Dayher Marchan <35051234+dayher@users.noreply.github.com>
2023-07-24 19:53:01 -0300

    Create index.py
    Pseudocodigo inicial

---
+++ index.py
@@ -0,0 +1,6 @@
+Pseudocódigo:
+
+Iniciar el cronómetro
+Iniciar un acumuladores de tiempo positivo y negativo
+Preparar interruptor de intervalo (Event Listener)
+Añadir a un arreglo de pares ordenados el intervalo de tiempo medido y el valor actual de acumulador p - n 
